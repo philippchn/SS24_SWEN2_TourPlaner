df7f6c952aedcea48a9bebe2a62727178dab127d
Li <liwen.wang1206@gmail.com>
2024-06-22 17:13:37 +0200

    update postgres_setup.sqlsetup

--- postgres_setup.sql
+++ postgres_setup.sql
@@ -6,24 +6,24 @@ DROP TABLE IF EXISTS t_tourLogs;
 DROP TABLE IF EXISTS t_tours;
 
 CREATE TABLE t_tours (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(255),
-    "tour_description" VARCHAR(255),
-    "start_location" VARCHAR(255),
-    "end_location" VARCHAR(255),
-    "transport_type" VARCHAR(255),
-    "distance" BIGINT,
-    "estimated_time" VARCHAR(255),
-    "route_information" TEXT
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    tour_description VARCHAR(255),
+    start_location VARCHAR(255),
+    end_location VARCHAR(255),
+    transport_type VARCHAR(255),
+    distance BIGINT,
+    estimated_time VARCHAR(255),
+    route_information TEXT
 );
 
 CREATE TABLE t_tourLogs (
-    "id" SERIAL PRIMARY KEY,
-    "date" TIMESTAMP,
-    "comment" VARCHAR(255),
-    "difficulty" VARCHAR(255),
-    "total_distance" BIGINT,
-    "total_time" VARCHAR(255),
-    "rating" VARCHAR(255),
-    "fk_tour" INTEGER REFERENCES t_tours(id)
+    id SERIAL PRIMARY KEY,
+    date DATE, -- Use DATE type to store LocalDate
+    comment VARCHAR(255),
+    difficulty INTEGER, -- Use INTEGER to store Integer
+    total_distance DOUBLE PRECISION, -- Use DOUBLE PRECISION to store Double
+    total_time BIGINT, -- Use BIGINT to store Long
+    rating INTEGER, -- Use INTEGER to store Integer
+    fk_tour INTEGER REFERENCES t_tours(id)
 );
